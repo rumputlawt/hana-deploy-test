a2c9473335c79b94ecd72d3080579f02761ac643
sera <serotoninoy@gmail.com>
2024-12-26 20:06:50 +0700

    fix: deploy api accessible by unauthorized users (#4)
    * throw unauthorized error
    
    * node types update

--- routes/api/deploy.ts
+++ routes/api/deploy.ts
@@ -5,28 +5,34 @@ import { STATUS_CODE } from "@std/http/status";
 
 export const handler = define.handlers({
 	async POST(ctx) {
-		const botId = Deno.env.get("BOT_ID");
-		const guildId = Deno.env.get("GUILD_ID");
-
-		if (!botId || !guildId) {
-			throw new HttpError(
-				STATUS_CODE.InternalServerError,
-				'Missing "BOT_ID" or "GUILD_ID" environment variable.',
-			);
+		if (!ctx.state.member) {
+			throw new HttpError(STATUS_CODE.Unauthorized);
 		} else {
-			try {
-				const commands = await loadCommands(ctx.config.mode);
-				await bot.applicationCommands
-					.bulkOverwriteGuildCommands(
-						botId,
-						guildId,
-						commands.map((command) => command.data),
-					);
-				return new Response(null, { status: STATUS_CODE.NoContent });
-			} catch (err) {
-				return Response.json(err, {
-					status: STATUS_CODE.InternalServerError,
-				});
+			const botId = Deno.env.get("BOT_ID");
+			const guildId = Deno.env.get("GUILD_ID");
+
+			if (!botId || !guildId) {
+				throw new HttpError(
+					STATUS_CODE.InternalServerError,
+					'Missing "BOT_ID" or "GUILD_ID" environment variable.',
+				);
+			} else {
+				try {
+					const commands = await loadCommands(ctx.config.mode);
+					await bot.applicationCommands
+						.bulkOverwriteGuildCommands(
+							botId,
+							guildId,
+							commands.map((command) => command.data),
+						);
+					return new Response(null, {
+						status: STATUS_CODE.NoContent,
+					});
+				} catch (err) {
+					return Response.json(err, {
+						status: STATUS_CODE.InternalServerError,
+					});
+				}
 			}
 		}
 	},
